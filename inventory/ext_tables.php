7930700de083ef45ae77ef07c86d15b618923de9
marco auenhammer <marco.auenhammer@gmail.com>
2017-02-19 12:35:54 +0100

    database access

---
+++ inventory/ext_tables.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: marco
+ * Date: 19.02.17
+ * Time: 12:31
+ */
+
+if (!defined ('TYPO3_MODE')) die ('Access denied.');
+
+$TCA['tx_inventory_domain_model_product'] = array (
+    'ctrl' => array (
+        'title' => 'Inventory',
+        'label' => 'name',
+    ),
+    'columns' => array(
+        'name' => array(
+            'label' => 'Item Label',
+            'config' => array(
+                'type' => 'input',
+                'size' => '20',
+                'eval' => 'trim,required'
+            )
+        ),
+        'description' => array(
+            'label' => 'Item Description',
+            'config' => array(
+                'type' => 'text',
+                'eval' => 'trim'
+            )
+        ),
+        'quantity' => array(
+            'label' => 'Stock Quantity',
+            'config' => array(
+                'type' => 'input',
+                'size' => '4',
+                'eval'=> 'int'
+            )
+        ),
+    ),
+    'types' => array(
+        '0' => array('showitem' => 'name, description, quantity')
+    )
+);
